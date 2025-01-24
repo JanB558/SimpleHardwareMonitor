5c371066e37bb8d99fb68f56aa1c000d0ce32550
JanB558 <72133201+JanB558@users.noreply.github.com>
2025-01-24 14:56:18 +0100

    Add project files.

---
+++ SimpleHardwareMonitor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SimpleHardwareMonitor.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ SimpleHardwareMonitor/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SimpleHardwareMonitor.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
