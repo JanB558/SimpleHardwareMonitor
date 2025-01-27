b8ce8bf70ef3ef8e7a36d11671fbc37bd36f356b
JanB558 <72133201+JanB558@users.noreply.github.com>
2025-01-27 15:37:12 +0100

    added converter for sensor list

---
+++ SimpleHardwareMonitor/Converters/SensorStatusConverter.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHardwareMonitor.Converters
+{
+    internal class SensorStatusConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            bool isEmpty = value == null || (value is IEnumerable enumerable && !enumerable.GetEnumerator().MoveNext());
+            return isEmpty ? "Sensors not found" : string.Empty;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
