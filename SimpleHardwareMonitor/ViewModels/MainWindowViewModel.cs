ea5a6914f770c8af58141b938509712a78230e48
JanB558 <72133201+JanB558@users.noreply.github.com>
2025-01-27 14:11:50 +0100

    finished left column

--- SimpleHardwareMonitor/ViewModels/MainWindowViewModel.cs
+++ SimpleHardwareMonitor/ViewModels/MainWindowViewModel.cs
@@ -29,22 +29,47 @@ namespace SimpleHardwareMonitor.ViewModels
         //Implementing INotifyPropertyChanged is too complicated since ISensor 
         //comes from nuget and cannot be directly edited (maybe could be with wrapper).
         //There are probably other solutions to this problem.
+        #region cpu
         public float? CpuTemperature => Hardware?.CPUTemperature?.Value;
         public float? CpuLoad => Hardware?.CPULoad?.Value;
         public ObservableCollection<IndexValue> ThreadsLoad
             => new ObservableCollection<IndexValue>(
-                Hardware?.ThreadsLoad?.Select((x, index) => new IndexValue
+                Hardware.ThreadsLoad.Select((x, index) => new IndexValue
                 {
                     Value = x.Value ?? 0f,
                     Index = index
                 }).ToList() ?? new List<IndexValue>()
                 );
+        #endregion
+        #region mobo
+        public ObservableCollection<IndexValue> MotherboardTemperature
+            => new ObservableCollection<IndexValue>(
+                Hardware.MotherboardTemperature.Select((x, index) => new IndexValue
+                {
+                    Value = x.Value ?? 0f,
+                    Index = index
+                }).ToList() ?? new List<IndexValue>()
+                );
+        #endregion
+        #region memory
+        public float? MemoryTemperature => Hardware?.MemoryTemperature?.Value;
+        public float? MemoryUsed => Hardware?.MemoryUsed?.Value;
+        public float? MemoryFree => Hardware?.MemoryFree?.Value;
+        public float? MemoryLoad => Hardware?.MemoryLoad?.Value;
+        public int? MemoryTotal => Hardware?.MemoryTotal;
+        #endregion
 
         private void RefreshHardwareInfo(object? state)
         {
             OnPropertyChanged(nameof(CpuTemperature));
             OnPropertyChanged(nameof(CpuLoad));
             OnPropertyChanged(nameof(ThreadsLoad));
+            OnPropertyChanged(nameof(MotherboardTemperature));
+            OnPropertyChanged(nameof(MemoryTemperature));
+            OnPropertyChanged(nameof(MemoryUsed));
+            OnPropertyChanged(nameof(MemoryFree));
+            OnPropertyChanged(nameof(MemoryLoad));
+            //OnPropertyChanged(nameof(MemoryTotal)); this property is constant
         }
     }
 }
