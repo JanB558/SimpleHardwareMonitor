ed088d04658cb2a5e2ac459097a9c91f3cb4982c
JanB558 <72133201+JanB558@users.noreply.github.com>
2025-01-27 13:56:57 +0100

    handled displaying list

--- SimpleHardwareMonitor/ViewModels/MainWindowViewModel.cs
+++ SimpleHardwareMonitor/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using SimpleHardwareMonitor.Models;
 using SimpleHardwareMonitor.Services;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SimpleHardwareMonitor.ViewModels
@@ -9,7 +12,7 @@ namespace SimpleHardwareMonitor.ViewModels
     public partial class MainWindowViewModel : ObservableObject
     {
         private readonly IHardwareService _hardwareService;
-        private System.Threading.Timer _refreshTimer;
+        private readonly System.Threading.Timer _refreshTimer;
         public MainWindowViewModel(IHardwareService hardwareService)
         {
             _hardwareService = hardwareService;
@@ -21,13 +24,27 @@ namespace SimpleHardwareMonitor.ViewModels
         [ObservableProperty]
         private HardwareInfo _hardware;
 
+        //These properties are needed because when _hardware properties are updated,
+        //user interface is not informed about the update.
+        //Implementing INotifyPropertyChanged is too complicated since ISensor 
+        //comes from nuget and cannot be directly edited (maybe could be with wrapper).
+        //There are probably other solutions to this problem.
         public float? CpuTemperature => Hardware?.CPUTemperature?.Value;
         public float? CpuLoad => Hardware?.CPULoad?.Value;
+        public ObservableCollection<IndexValue> ThreadsLoad
+            => new ObservableCollection<IndexValue>(
+                Hardware?.ThreadsLoad?.Select((x, index) => new IndexValue
+                {
+                    Value = x.Value ?? 0f,
+                    Index = index
+                }).ToList() ?? new List<IndexValue>()
+                );
 
         private void RefreshHardwareInfo(object? state)
         {
             OnPropertyChanged(nameof(CpuTemperature));
             OnPropertyChanged(nameof(CpuLoad));
+            OnPropertyChanged(nameof(ThreadsLoad));
         }
     }
 }
